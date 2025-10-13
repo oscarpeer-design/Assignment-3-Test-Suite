203db42e9a1dbf0a09a64e89529a412601bb576d
oscarpeer-design <peeroscar.personal@gmail.com>
2025-10-14 10:38:23 +1100

    Update Assignment 3 Selenium Tests.py
    Ensured modifications to the main branch are reflected in the backup branch.

--- Assignment 3 Selenium Tests.py
+++ Assignment 3 Selenium Tests.py
@@ -10,7 +10,7 @@ class TestSuite():
         self.driver = driver
         self.driver.get(weblink)
 
-        self.email = "PeerOscar.Personal@gmail.com" #use an actual email
+        self.email = "peeroscar.personal@gmail.com" #use an actual email
         self.password = "Happy_Hacker"
         self.phone = "0449570899"
 
@@ -26,6 +26,9 @@ class TestSuite():
         except:
             pass
     
+    def save_screenshot(self, screenshot_name = "screenshot.png"):
+        self.driver.save_screenshot(screenshot_name)
+
     def element_by_id(self, id):
         try:
             return self.driver.find_element(by = By.ID, value = id)
@@ -69,20 +72,39 @@ class TestSuite():
         self.wait(1)
         chk_agree = self.element_by_id("input-agree")
         print(chk_agree)
-        driver.execute_script("arguments[0].click();", chk_agree)
+        self.driver.execute_script("arguments[0].click();", chk_agree)
         btn_submit = self.element_by_css("input.btn.btn-primary")
         btn_submit.click()
         
 
-        successful_login_url = "https://ecommerce-playground.lambdatest.io/index.php?route=account/success"
+        successful_registration_url = "https://ecommerce-playground.lambdatest.io/index.php?route=account/success"
         self.set_current_url()
-        assert self.current_url == successful_login_url, f"❌ URL mismatch: expected {successful_login_url}, got {self.current_url}"
+        self.save_screenshot("login.png")
+        assert self.current_url == successful_registration_url, f"❌ URL mismatch: expected {successful_registration_url}, got {self.current_url} \nThis means the login was unsuccessful"
         print("✅ URL is correct!")
         self.wait(3)
 
+    def test_login_valid(self):
+        self.driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/login")
+        input_email = self.element_by_name("email")
+        input_email.send_keys(self.email)
+        input_password = self.element_by_id("input-password")
+        input_password.send_keys(self.password)
+        self.wait(2)
+        btn_login = self.element_by_css("input[value='Login']")
+        self.driver.execute_script("arguments[0].click();", btn_login)
+        self.wait(3)
+        successful_login_url = "https://ecommerce-playground.lambdatest.io/index.php?route=account/account"
+        self.set_current_url()
+        assert self.current_url == successful_login_url, f"❌ URL mismatch: expected {successful_login_url}, got {self.current_url} \nThis means the login was unsuccessful"
+        print("✅ Login was successful!")
+
+    def test_login_invalid(self):
+        pass
+
     def run_tests(self):
-        self.test_registration()
-        #self.test3()
+        #self.test_registration()
+        self.test_login_valid()
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
